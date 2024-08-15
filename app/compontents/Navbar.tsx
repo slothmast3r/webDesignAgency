3fc5f678739accdeadc7a88f3433241432e56f41
slothmast3r <oskar.straszynski@gmail.com>
2024-08-15 23:28:15 +0200

    navigation added

---
+++ app/compontents/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const navigationLinks = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/services", label: "Services" },
+  { href: "/contact", label: "Contact" },
+];
+
+function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+
+        {navigationLinks.map((link) => (
+          <NavigationMenuItem key={link.href}>
+            <Link href={link.href} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {link.label}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+      
+    </NavigationMenu>
+  );
+}
+
+export default Navbar;
