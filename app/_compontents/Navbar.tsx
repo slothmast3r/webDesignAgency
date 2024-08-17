c1296b1e950b8047a4125388c0f68cfa848153d7
slothmast3r <oskar.straszynski@gmail.com>
2024-08-17 17:40:08 +0200

    navbar somewhat done

--- app/_compontents/Navbar.tsx
+++ app/_compontents/Navbar.tsx
@@ -17,55 +17,70 @@ import Link from "next/link";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { MenuIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const navigationLinks = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   { href: "/services", label: "Services" },
-  { href: "/contact", label: "Contact" },
 ];
 
 function Navbar() {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 800px)");
 
   return (
-    <NavigationMenu>
-      {isDesktop ? (
-        <NavigationMenuList>
-          {navigationLinks.map((link) => (
-            <NavigationMenuItem key={link.href}>
-              <Link href={link.href} legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  {link.label}
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      ) : (
-        <Drawer direction="right">
-          <DrawerTrigger>
-            <MenuIcon />
-          </DrawerTrigger>
-          <DrawerContent>
-            {" "}
-            <NavigationMenuList className="flex flex-col justify-start items-start">
-              {navigationLinks.map((link) => (
-                <NavigationMenuItem key={link.href}>
-                  <Link href={link.href} legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      {link.label}
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </DrawerContent>
-        </Drawer>
+    <header className="p-5 m-auto max-w-[72rem] flex justify-between">
+      <div>LOGO</div>
+      <NavigationMenu className="">
+        {isDesktop ? (
+          <NavigationMenuList>
+            {navigationLinks.map((link) => (
+              <NavigationMenuItem key={link.href}>
+                <Link href={link.href} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {link.label}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        ) : (
+          <Drawer direction="right">
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent className="flex flex-col">
+              <div>LOGO</div>
+              <NavigationMenuList className="flex flex-col justify-start items-start h-fit">
+                {navigationLinks.map((link) => (
+                  <NavigationMenuItem key={link.href}>
+                    <Link href={link.href} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        {link.label}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+              <Button className="w-fit">
+                <Link href="/contact" passHref>
+                  Contact us
+                </Link>
+              </Button>
+            </DrawerContent>
+          </Drawer>
+        )}
+      </NavigationMenu>
+      {isDesktop && (
+        <Button>
+          <Link href="/contact" passHref>
+            Contact us
+          </Link>
+        </Button>
       )}
-    </NavigationMenu>
+    </header>
   );
 }
 
